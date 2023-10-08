aec1b58bf6fa1c588e6b22590f3fd82e079eedad
Gunnitsingh <92669327+Gunnitsingh@users.noreply.github.com>
2023-10-08 18:37:38 +0530

    end of section 9

--- API/Migrations/20230810174456_Update-UserNew.Designer.cs
+++ API/Migrations/20230810174456_Update-UserNew.Designer.cs
@@ -4,6 +4,7 @@ using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230810174456_Update-UserNew")]
+    partial class UpdateUserNew
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -45,7 +48,7 @@ namespace API.Migrations
                     b.Property<string>("Gender")
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("Interests")
+                    b.Property<string>("Interest")
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Introduction")
---
+++ API/Migrations/20230810181136_Update-UserInterests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserInterests : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Interest",
+                table: "Users",
+                newName: "Interests");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Interests",
+                table: "Users",
+                newName: "Interest");
+        }
+    }
+}
